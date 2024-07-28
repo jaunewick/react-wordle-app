e5abd8aa9d7ae1d4157ee692e526d5e1e7005469
Daniel Giao <daniel.giao@icloud.com>
2024-07-28 03:34:27 -0400

    Added shortcut indication and cursor fix

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
   return (
     <div className="App">
       <h1>WORDLE</h1>
+      <p>Hold the <i>Alt</i> or <i>Option</i> key to reveal solution</p>
       {solution && <Wordle solution={solution} />}
     </div>
   );
